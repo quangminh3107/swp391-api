05b7426eb195d613b163864329761851d8cc136b
quangminh3107 <satthuzombje@gmail.com>
2026-01-26 14:06:06 +0700

    Update WebSocketConfig.java

--- src/main/java/com/devmam/taraacademyapi/config/WebSocketConfig.java
+++ src/main/java/com/devmam/taraacademyapi/config/WebSocketConfig.java
@@ -1,29 +1,29 @@
-package com.devmam.taraacademyapi.config;
+// package com.devmam.taraacademyapi.config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+// import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+// import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+// import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
-@Configuration
-@EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+// @Configuration
+// @EnableWebSocketMessageBroker
+// public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        // Enable a simple memory-based message broker
-        registry.enableSimpleBroker("/topic");
+//     @Override
+//     public void configureMessageBroker(MessageBrokerRegistry registry) {
+//         // Enable a simple memory-based message broker
+//         registry.enableSimpleBroker("/topic");
 
-        // Prefix for messages from client
-        registry.setApplicationDestinationPrefixes("/app");
-    }
+//         // Prefix for messages from client
+//         registry.setApplicationDestinationPrefixes("/app");
+//     }
 
-    @Override
-    public void registerStompEndpoints(StompEndpointRegistry registry) {
-        // Register STOMP endpoint
-        registry.addEndpoint("/ws")
-                .setAllowedOriginPatterns("*")
-                .withSockJS();
-    }
-}
+//     @Override
+//     public void registerStompEndpoints(StompEndpointRegistry registry) {
+//         // Register STOMP endpoint
+//         registry.addEndpoint("/ws")
+//                 .setAllowedOriginPatterns("*")
+//                 .withSockJS();
+//     }
+// }
